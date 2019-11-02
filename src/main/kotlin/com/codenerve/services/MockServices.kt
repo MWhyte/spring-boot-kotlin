da4fcf3d0a1116dd857d65105ad520ca965eb203
mwhyte <mrmwhyte@gmail.com>
2019-11-02 21:06:00 +0000

    learning kotlin

--- src/main/kotlin/com/codenerve/services/MockServices.kt
+++ src/main/kotlin/com/codenerve/services/MockServices.kt
@@ -8,14 +8,18 @@ class MockServices {
 
     val restTemplate: RestTemplate = RestTemplate()
 
-    fun allEmployees(): List<String> {
+    fun allEmployees(): List<Employee> {
         //TODO
         return emptyList()
     }
-
 
     fun getEmployeeById(id: String) {
         //TODO
     }
 
+    data class Employee(val id: Long,
+                        val employeeName: String,
+                        val employeeSalary: String,
+                        val employeeAge: String,
+                        val profileImage: String)
 }
