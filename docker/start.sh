c880106d6674d9652f0db7052236a5c7be5091fe
mwhyte <mrmwhyte@gmail.com>
2020-02-09 22:32:01 +0000

    adding ability to push stats to prometheus and grafana

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+PROJECT_DIR=/Users/mrwhyte/Dropbox/Development/Code/Kotlin/kotlin-microservice
+
 docker run -d -p 9411:9411 --name zipkin openzipkin/zipkin
-docker run -d -p 9090:9090 -v /Users/mrwhyte/Dropbox/Development/Code/Kotlin/kotlin-microservice/docker/prometheus.yml:/etc/prometheus/prometheus.yml --name prometheus prom/prometheus
+docker run -d -p 9090:9090 -v ${PROJECT_DIR}/docker/config/prometheus.yml:/etc/prometheus/prometheus.yml --name prometheus prom/prometheus
 docker run -d -p 3000:3000 --name grafana grafana/grafana
 
-./seeall.sh;
+docker ps -a
--- docker/stop.sh
+++ docker/stop.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-docker container stop zipkin;
-docker container stop prometheus;
-docker container stop grafana;
+docker container stop zipkin
+docker container stop prometheus
+docker container stop grafana
 
-docker container rm zipkin;
-docker container rm prometheus;
-docker container rm grafana;
+docker container rm zipkin
+docker container rm prometheus
+docker container rm grafana
 
-./seeall.sh;
+docker ps -a
